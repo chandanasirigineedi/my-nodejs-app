30c3c9ce35c83dce9cf897c3cdfdc3c72231c518
chandana <84661960+chandanasirigineedi@users.noreply.github.com>
2024-11-26 13:23:59 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./Style/Style.css"
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { AuthProvider } from "./context/AuthProvider";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import 'index.css';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
-const base = process.env.REACT_APP_BASE_PATH
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={base}>
-      <AuthProvider>
-        <Routes>
-          <Route path="/*" element={<App />} />
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
-    </React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
